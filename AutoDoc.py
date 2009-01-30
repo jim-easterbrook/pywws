5a51a5a5db1c66171fb1190cb6f200159327eb43
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2009-01-30 14:24:12 +0000

    AutoDoc.py : an attempt to use pydoc to generate documentation

---
+++ AutoDoc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import pydoc
+
+doc_dir = 'auto_doc'
+if not os.path.isdir(doc_dir):
+    os.mkdir(doc_dir)
+wd = os.getcwd()
+for file in os.listdir('./'):
+    base, ext = os.path.splitext(file)
+    if ext != '.py':
+        continue
+    pydoc.writedoc(base)
+    src_file = base + '.html'
+    if not os.path.exists(src_file):
+        continue
+    # post-process pydoc output to clean up some of its eccentricities
+    dest_file = os.path.join(doc_dir, src_file)
+    src = open(src_file, 'r')
+    dest = open(dest_file, 'w')
+    for line in src:
+        if os.path.join(wd, file) in line:
+            line = line.replace(os.path.join(wd, file), os.path.join('..', file))
+        if '"."' in line:
+            line = line.replace('"."', 'index.html')
+        dest.write(line)
+    dest.close()
+    src.close()
+    os.unlink(src_file)
