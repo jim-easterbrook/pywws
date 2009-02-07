643a645f5d17cb6cbc7867bc0503cae4a6fab172
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2009-02-07 11:30:01 +0000

    ToTwitter.py : now sets source attribute when posting.

--- ToTwitter.py
+++ ToTwitter.py
@@ -5,7 +5,7 @@ Post a message to Twitter.
 
 usage: python ToTwitter.py [options] data_dir file
 options are:
-\t-h or --help\t\tdisplay this help
+\t--help\t\tdisplay this help
 data_dir is the root directory of the weather data
 file is the text file to be uploaded
 
@@ -28,6 +28,7 @@ def ToTwitter(params, file):
     tweet_file.close()
     if len(tweet) > 0:
         api = twitter.Api(username=username, password=password)
+        api.SetSource('pywws')
         status = api.PostUpdate(codecs.decode(tweet, 'iso-8859-1'))
     return 0
 def main(argv=None):
@@ -39,16 +40,16 @@ def main(argv=None):
         print >>sys.stderr, 'Error: %s\n' % msg
         print >>sys.stderr, __doc__.strip()
         return 1
-    # check arguments
-    if len(args) != 2:
-        print >>sys.stderr, "Error: 2 arguments required"
-        print >>sys.stderr, __doc__.strip()
-        return 2
     # process options
     for o, a in opts:
         if o == '--help':
             print __doc__.strip()
             return 0
+    # check arguments
+    if len(args) != 2:
+        print >>sys.stderr, "Error: 2 arguments required"
+        print >>sys.stderr, __doc__.strip()
+        return 2
     return ToTwitter(DataStore.params(args[0]), args[1])
 if __name__ == "__main__":
     sys.exit(main())
