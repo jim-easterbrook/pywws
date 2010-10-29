5502037430772f92362d292709a199a242bbf0f8
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2010-10-29 14:48:35 +0000

    Removed stuff to compile language files from setup.py - it's better done by a makefile.

--- setup.py
+++ setup.py
@@ -1,44 +1,8 @@
 #!/usr/bin/env python
 
 from datetime import date
-from distutils.command.build import build
-from distutils.core import setup, Command
-from distutils import log
-import os
-from subprocess import check_call, Popen, PIPE
-import sys
-if sys.platform == 'win32':
-    sys.path.append(os.path.join(sys.prefix, 'Tools', 'i18n'))
-    import msgfmt
-
-class build_langs(Command):
-    description = 'Compile language localisation files.'
-    user_options = []
-    def initialize_options(self):
-    	pass
-    def finalize_options(self):
-    	pass
-    def run(self):
-        # compile language files
-        srcDir = os.path.join(os.path.dirname(sys.argv[0]), 'languages')
-        langRoot = os.path.join(os.path.dirname(sys.argv[0]), 'locale')
-        for file in os.listdir(srcDir):
-            base, ext = os.path.splitext(file)
-            if ext.lower() != '.po':
-                continue
-            src = os.path.join(srcDir, file)
-            dest = os.path.join(langRoot, base, 'LC_MESSAGES', 'pywws.mo')
-            if os.path.exists(dest) and os.path.getctime(dest) > os.path.getctime(src):
-                continue
-            log.info("Creating %s" % (dest))
-            dest_dir = os.path.dirname(dest)
-            if not os.path.isdir(dest_dir):
-                os.makedirs(dest_dir)
-            if sys.platform == 'win32':
-                msgfmt.make(src, dest)
-            else:
-                check_call(['msgfmt', '--output-file=%s' % dest, src])
-build.sub_commands.insert(0, ('build_langs', None))
+from distutils.core import setup
+from subprocess import Popen, PIPE
 
 revision = 0
 for line in Popen(['svn', 'info'], stdout=PIPE).stdout:
@@ -53,5 +17,4 @@ setup(name='pywws',
       author='Jim Easterbrook',
       author_email='jim@jim-easterbrook.me.uk',
       url='http://code.google.com/p/pywws/',
-      cmdclass={'build_langs': build_langs},
       )
