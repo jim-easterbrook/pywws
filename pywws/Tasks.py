04d2c42ad5bdf02b1f035f5cfcdf3977ec04f87f
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2010-11-27 11:59:49 +0000

    Added RegularTasks class to do hourly, daily, 12 hourly and 'live' tasks.

--- pywws/Tasks.py
+++ pywws/Tasks.py
@@ -2,11 +2,15 @@
 
 """Routines to perform common tasks such as plotting gaphs or uploading files."""
 
+from datetime import datetime, timedelta
 import logging
 import os
 
 from Plot import GraphPlotter
 import Template
+from TimeZone import Local, utc
+import ToUnderground
+import Upload
 from WindRose import RosePlotter
 
 def DoPlots(section, params, raw_data, hourly_data, daily_data, monthly_data,
@@ -57,7 +61,7 @@ def DoTwitter(section, params, raw_data, hourly_data, daily_data, monthly_data,
         'paths', 'templates', os.path.expanduser('~/weather/templates/'))
     templates = eval(params.get(section, 'twitter', '[]'))
     if not templates:
-        return
+        return True
     import ToTwitter
     twitter = ToTwitter.ToTwitter(params, translation=translation)
     for template in templates:
@@ -68,5 +72,71 @@ def DoTwitter(section, params, raw_data, hourly_data, daily_data, monthly_data,
             params, raw_data, hourly_data, daily_data,
             monthly_data, input_file, output_file, translation=translation)
         logger.info("Tweeting")
-        twitter.UploadFile(output_file)
+        OK = twitter.UploadFile(output_file)
         os.unlink(output_file)
+        if not OK:
+            return False
+    return True
+class RegularTasks(object):
+    def __init__(self, params, raw_data, hourly_data, daily_data, monthly_data,
+                 translation):
+        self.params = params
+        self.raw_data = raw_data
+        self.hourly_data = hourly_data
+        self.daily_data = daily_data
+        self.monthly_data = monthly_data
+        self.translation = translation
+        # create a ToUnderground object
+        self.underground = ToUnderground.ToUnderground(self.params, self.raw_data)
+        # get local time's offset from UTC, without DST
+        now = self.raw_data.before(datetime.max)
+        time_offset = Local.utcoffset(now) - Local.dst(now)
+        # get daytime end hour, in UTC
+        self.day_end_hour = eval(params.get('config', 'day end hour', '21'))
+        self.day_end_hour = (self.day_end_hour - (time_offset.seconds / 3600)) % 24
+    def do_tasks(self):
+        sections = ['live']
+        now = self.raw_data.before(datetime.max)
+        threshold = now.replace(minute=0, second=0, microsecond=0)
+        last_update = self.params.get_datetime('hourly', 'last update')
+        if (not last_update) or (last_update < threshold):
+            # time to do hourly tasks
+            sections.append('hourly')
+            # set 12 hourly threshold
+            threshold -= timedelta(hours=(threshold.hour - self.day_end_hour) % 12)
+            last_update = self.params.get_datetime('12 hourly', 'last update')
+            if (not last_update) or (last_update < threshold):
+                # time to do 12 hourly tasks
+                sections.append('12 hourly')
+            # set daily threshold
+            threshold -= timedelta(hours=(threshold.hour - self.day_end_hour) % 24)
+            last_update = self.params.get_datetime('daily', 'last update')
+            if (not last_update) or (last_update < threshold):
+                # time to do daily tasks
+                sections.append('daily')
+        uploads = []
+        OK = True
+        for section in sections:
+            if not DoTwitter(
+                section, self.params, self.raw_data, self.hourly_data,
+                self.daily_data, self.monthly_data, self.translation):
+                OK = False
+                break
+            if eval(self.params.get(section, 'underground', 'False')):
+                if not self.underground.Upload(True):
+                    OK = False
+                    break
+            uploads += DoPlots(
+                section, self.params, self.raw_data, self.hourly_data,
+                self.daily_data, self.monthly_data, self.translation)
+            uploads += DoTemplates(
+                section, self.params, self.raw_data, self.hourly_data,
+                self.daily_data, self.monthly_data, self.translation)
+        if uploads:
+            OK = OK and Upload.Upload(self.params, uploads) == 0
+            for file in uploads:
+                os.unlink(file)
+        if OK:
+            for section in sections:
+                self.params.set(section, 'last update', now.isoformat(' '))
+        return OK
