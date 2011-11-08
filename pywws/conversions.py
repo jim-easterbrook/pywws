635d46d46d7f3f747378366314c0ae75fa8aaaf5
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2011-11-08 17:30:21 +0000

    Made unit conversions able to cope with 'None' values.

--- pywws/conversions.py
+++ pywws/conversions.py
@@ -5,30 +5,44 @@ other popular units.
 
 def pressure_inhg(hPa):
     "Convert pressure from hectopascals/millibar to inches of mercury"
+    if hPa is None:
+        return None
     return hPa / 33.86389
 
 def rain_inch(mm):
     "Convert rainfall from millimetres to inches"
+    if mm is None:
+        return None
     return mm / 25.4
 
 def temp_f(c):
     "Convert temperature from Celsius to Fahrenheit"
+    if c is None:
+        return None
     return (c * 9.0 / 5.0) + 32.0
 
 def wind_kmph(ms):
     "Convert (wind) wind from metres per second to kilometres per hour"
+    if ms is None:
+        return None
     return ms * 3.6
 
 def wind_mph(ms):
     "Convert (wind) wind from metres per second to miles per hour"
+    if ms is None:
+        return None
     return ms * 3.6 / 1.609344
 
 def wind_kn(ms):
     "Convert (wind) wind from metres per second to knots"
+    if ms is None:
+        return None
     return ms * 3.6 / 1.852
 
 def wind_bft(ms):
     "Convert (wind) wind from metres per second to Beaufort scale"
+    if ms is None:
+        return None
     if ms < 0.3:
         return 0
     elif ms <= 1.5:
