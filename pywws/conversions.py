2df963923c1757948568db060e014e360c76ce7e
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2012-08-31 13:02:50 +0000

    Added a winddir_text() function to the conversions.py module. Rewrote wind_bft() function. Added a simple test script.

--- pywws/conversions.py
+++ pywws/conversions.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
+
+"""conversions.py - a set of functions to convert pywws native units
+(Centigrade, mm, m/s, hPa) to other popular units
+
 """
-A set of functions to convert pywws native units (Centigrade, mm, m/s, hPa) to
-other popular units.
-"""
+
+# rename imports to prevent them being imported when
+# doing 'from pywws.conversions import *'
+import Localisation as _Localisation
 
 def illuminance_wm2(lux):
     "Approximate conversion of illuminance in lux to solar radiation in W/m2"
@@ -33,6 +39,23 @@ def winddir_degrees(pts):
         return None
     return pts * 22.5
 
+_winddir_text_array = None
+
+def winddir_text(pts):
+    "Convert wind direction from 0..15 to compass point text"
+    global _winddir_text_array
+    if pts is None:
+        return None
+    if not _winddir_text_array:
+        _ = _Localisation.translation.gettext
+        _winddir_text_array = (
+            _('N'), _('NNE'), _('NE'), _('ENE'),
+            _('E'), _('ESE'), _('SE'), _('SSE'),
+            _('S'), _('SSW'), _('SW'), _('WSW'),
+            _('W'), _('WNW'), _('NW'), _('NNW'),
+            )
+    return _winddir_text_array[pts]
+
 def wind_kmph(ms):
     "Convert wind from metres per second to kilometres per hour"
     if ms is None:
@@ -51,32 +74,32 @@ def wind_kn(ms):
         return None
     return ms * 3.6 / 1.852
 
+_bft_threshold = (
+    0.3, 1.5, 3.4, 5.4, 7.9, 10.7, 13.8, 17.1, 20.7, 24.4, 28.4, 32.6)
+
 def wind_bft(ms):
     "Convert wind from metres per second to Beaufort scale"
     if ms is None:
         return None
-    if ms < 0.3:
-        return 0
-    elif ms <= 1.5:
-        return 1
-    elif ms <= 3.4:
-        return 2
-    elif ms <= 5.4:
-        return 3
-    elif ms <= 7.9:
-        return 4
-    elif ms <= 10.7:
-        return 5
-    elif ms <= 13.8:
-        return 6
-    elif ms <= 17.1:
-        return 7
-    elif ms <= 20.7:
-        return 8
-    elif ms <= 24.4:
-        return 9
-    elif ms <= 28.4:
-        return 10
-    elif ms <= 32.6:
-        return 11
-    return 12
+    for bft in range(len(_bft_threshold)):
+        if ms < _bft_threshold[bft]:
+            return bft
+    return len(_bft_threshold)
+
+if __name__ == "__main__":
+    # run some simple tests
+    print 'Wind speed:'
+    print '%6s %8s %8s %8s %6s' % ('m/s', 'km/h', 'mph', 'knots', 'bft')
+    for ms in (0, 1, 2, 4, 6, 9, 12, 15, 18, 22, 26, 30, 34):
+        print '%6g %8.3f %8.3f %8.3f %6d' % (
+            ms, wind_kmph(ms), wind_mph(ms), wind_kn(ms), wind_bft(ms))
+    print 'Wind direction:'
+    for pts in range(16):
+        print winddir_text(pts),
+    print
+    print 'Wind direction, in Swedish:'
+    _Localisation.SetTranslation('sv')
+    _winddir_text_array = None
+    for pts in range(16):
+        print winddir_text(pts),
+    print
