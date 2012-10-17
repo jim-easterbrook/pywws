bc08327531d43eccd469891a420ea98c1139e42d
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2012-10-17 18:17:08 +0000

    Added information to setup.py to improve PyPI listing.

--- setup.py
+++ setup.py
@@ -9,6 +9,24 @@ setup(name='pywws',
       author='Jim Easterbrook',
       author_email='jim@jim-easterbrook.me.uk',
       url='http://code.google.com/p/pywws/',
+      long_description="""
+A collection of Python scripts to read, store and process data from
+popular USB wireless weather stations such as Elecsa AstroTouch 6975,
+Watson W-8681, WH-1080PC, WH1080, WH1081, WH3080 etc. I assume any
+model that is supplied with the EasyWeather Windows software is
+compatible, but cannot guarantee this.
+
+The software has been developed to run in a low power, low memory
+environment such as a router. It can be used to create graphs and web
+pages showing recent weather readings, typically updated every hour.
+""",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2 :: Only',
+          ],
       packages=['pywws'],
       package_data={'pywws' : ['services/*.ini']},
       )
