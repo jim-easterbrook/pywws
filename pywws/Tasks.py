jim-easterbrook/pywws

commit ca0d137ac5ef465200929a4c468c363f03269cdd

Removed toservice.py "rapid fire" mode.
There is now a second config file for Weather Underground's rapid fire
mode. Use 'underground' for normal updates, 'underground_rf' for rapid
fire. The documentation has been extended to cover this in more detail.

pywws/Tasks.py
@@ -116,7 +116,7 @@ class RegularTasks(object):
             if not self.do_twitter(template, data):
                 OK = False
         for service in eval(self.params.get('live', 'services', '[]')):
-            self.services[service].RapidFire(data, True)
+            self.services[service].Upload(data)
         uploads = []
         local_files = []
         for template, flags in self._parse_templates('live', 'plot'):

pywws/toservice.py
@@ -65,8 +65,8 @@ If you haven't already done so, visit the organisation's web site and
 create an account for your weather station. Make a note of any site ID
 and password details you are given.
 
-Stop any pywws software that is running and then run ``toservice``
-to create a section in ``weather.ini``::
+Stop any pywws software that is running and then run ``toservice`` to
+create a section in ``weather.ini``::
 
     python -m pywws.toservice data_dir service_name
 
@@ -111,7 +111,7 @@ section, depending on how often you want to send data. For example::
     twitter = []
     plot = []
     text = []
-    services = ['underground']
+    services = ['underground_rf']
 
     [logged]
     twitter = []
@@ -126,13 +126,13 @@ section, depending on how often you want to send data. For example::
     services = ['underground']
 
 Note that the ``[live]`` section is only used when running
-``LiveLog.py``. It is a good idea to repeat any service selected in
-``[live]`` in the ``[logged]`` or ``[hourly]`` section in case you
-switch to running :mod:`Hourly`.
+:py:mod:`~pywws.LiveLog`. It is a good idea to repeat any service
+selected in ``[live]`` in the ``[logged]`` or ``[hourly]`` section in
+case you switch to running :py:mod:`~pywws.Hourly`.
 
-Restart your regular pywws program (:mod:`Hourly` or :mod:`LiveLog`)
-and visit the appropriate web site to see regular updates from your
-weather station.
+Restart your regular pywws program (:py:mod:`~pywws.Hourly` or
+:py:mod:`~pywws.LiveLog`) and visit the appropriate web site to see
+regular updates from your weather station.
 
 Notes on the services
 ---------------------
@@ -232,7 +232,7 @@ class ToService(object):
     Underground.
 
     """
-    def __init__(self, params, status, calib_data, service_name=None):
+    def __init__(self, params, status, calib_data, service_name):
         """
 
         :param params: pywws configuration.
@@ -247,21 +247,19 @@ class ToService(object):
 
         :type calib_data: :class:`pywws.DataStore.calib_store`
 
-        :keyword service_name: name of service to upload to.
+        :param service_name: name of service to upload to.
 
         :type service_name: string
     
         """
-        if service_name:
-            self.config_section = service_name
-            self.logger = logging.getLogger(
-                'pywws.ToService(%s)' % service_name)
-        else:
-            self.logger = logging.getLogger(
-                'pywws.%s' % self.__class__.__name__)
+        self.logger = logging.getLogger('pywws.ToService(%s)' % service_name)
         self.params = params
         self.status = status
         self.data = calib_data
+        self.service_name = service_name
+        # 'derived' services such as 'underground_rf' share their
+        # parent's config and templates
+        config_section = self.service_name.split('_')[0]
         self.old_response = None
         self.old_ex = None
         # set default socket timeout, so urlopen calls don't hang forever
@@ -270,37 +268,25 @@ class ToService(object):
         service_params = SafeConfigParser()
         service_params.optionxform = str
         service_params.readfp(open(os.path.join(
-            os.path.dirname(__file__), 'services',
-            '%s.ini' % (self.config_section))))
+            os.path.dirname(__file__), 'services', '%s.ini' % (self.service_name))))
         # get URL
         self.server = service_params.get('config', 'url')
         # get fixed part of upload data
         self.fixed_data = dict()
         for name, value in service_params.items('fixed'):
             if value[0] == '*':
-                value = self.params.get(
-                    self.config_section, value[1:], 'unknown')
+                value = self.params.get(config_section, value[1:], 'unknown')
             self.fixed_data[name] = value
         # create templater
         self.templater = Template.Template(
             self.params, self.status, self.data, self.data, None, None,
             use_locale=False)
         self.template_file = os.path.join(
-            os.path.dirname(__file__), 'services',
-            '%s_template_%s.txt' % (service_name,
-                                    self.params.get('config', 'ws type')))
+            os.path.dirname(__file__), 'services', '%s_template_%s.txt' % (
+                config_section, self.params.get('config', 'ws type')))
         # get other parameters
         self.catchup = eval(service_params.get('config', 'catchup'))
         self.use_get = eval(service_params.get('config', 'use get'))
-        rapid_fire = eval(service_params.get('config', 'rapidfire'))
-        if rapid_fire:
-            self.server_rf = service_params.get('config', 'url-rf')
-            self.fixed_data_rf = dict(self.fixed_data)
-            for name, value in service_params.items('fixed-rf'):
-                self.fixed_data_rf[name] = value
-        else:
-            self.server_rf = self.server
-            self.fixed_data_rf = self.fixed_data
         self.expected_result = eval(service_params.get('config', 'result'))
 
     def translate_data(self, current, fixed_data):
@@ -427,20 +413,20 @@ class ToService(object):
         if catchup and self.catchup > 0:
             start = datetime.utcnow() - timedelta(days=self.catchup)
             last_update = self.params.get_datetime(
-                self.config_section, 'last update')
+                self.service_name, 'last update')
             if last_update:
-                self.params.unset(self.config_section, 'last update')
-                self.status.set('last update', self.config_section,
+                self.params.unset(self.service_name, 'last update')
+                self.status.set('last update', self.service_name,
                                 last_update.isoformat(' '))
             last_update = self.status.get_datetime(
-                'last update', self.config_section)
+                'last update', self.service_name)
             if last_update:
                 start = max(start, last_update + timedelta(minutes=1))
             count = 0
             for data in self.data[start:]:
                 if not self.send_data(data, self.server, self.fixed_data):
                     return False
-                self.status.set('last update', self.config_section,
+                self.status.set('last update', self.service_name,
                                 data['idx'].isoformat(' '))
                 count += 1
             if count:
@@ -452,60 +438,7 @@ class ToService(object):
                     self.data[last_update], self.server, self.fixed_data):
                 return False
             self.status.set(
-                'last update', self.config_section, last_update.isoformat(' '))
-        return True
-
-    def RapidFire(self, data, catchup):
-        """Upload a 'Rapid Fire' weather data record.
-
-        This method uploads either a single data record (typically one
-        obtained during 'live' logging), or all records since the last
-        upload (up to 7 days), according to the value of
-        :obj:`catchup`.
-
-        It sets the ``last update`` configuration value to the time
-        stamp of the most recent record successfully uploaded.
-
-        The :obj:`data` parameter contains the data to be uploaded.
-        It should be a 'calibrated' data record, as stored in
-        :class:`pywws.DataStore.calib_store`.
-
-        :param data: the weather data record.
-
-        :type data: dict
-
-        :param catchup: upload all data since last upload.
-
-        :type catchup: bool
-
-        :return: success status
-
-        :rtype: bool
-        
-        """
-        last_log = self.data.before(datetime.max)
-        if not last_log or last_log < data['idx'] - FIVE_MINS:
-            # logged data is not (yet) up to date
-            return True
-        if catchup and self.catchup > 0:
-            last_update = self.params.get_datetime(
-                self.config_section, 'last update')
-            if last_update:
-                self.params.unset(self.config_section, 'last update')
-                self.status.set('last update', self.config_section,
-                                last_update.isoformat(' '))
-            last_update = self.status.get_datetime(
-                'last update', self.config_section)
-            if not last_update:
-                last_update = datetime.min
-            if last_update <= last_log - FIVE_MINS:
-                # last update was well before last logged data
-                if not self.Upload(True):
-                    return False
-        if not self.send_data(data, self.server_rf, self.fixed_data_rf):
-            return False
-        self.status.set(
-            'last update', self.config_section, data['idx'].isoformat(' '))
+                'last update', self.service_name, last_update.isoformat(' '))
         return True
 
 def main(argv=None):
@@ -537,8 +470,7 @@ def main(argv=None):
     logger = ApplicationLogger(verbose)
     return ToService(
         DataStore.params(args[0]), DataStore.status(args[0]),
-        DataStore.calib_store(args[0]), service_name=args[1]
-        ).Upload(catchup)
+        DataStore.calib_store(args[0]), args[1]).Upload(catchup)
 
 if __name__ == "__main__":
     sys.exit(main())

pywws/version.py
@@ -1,3 +1,3 @@
-version = '13.05'
-release = '1014'
-commit = '484db9c'
+version = '13.06'
+release = '1016'
+commit = 'cdf2b40'

pywws/services/metoffice.ini
@@ -19,7 +19,6 @@
 [config]
 url		= http://wow.metoffice.gov.uk/automaticreading
 catchup		= 7
-rapidfire	= False
 use get		= False
 result		= []
 

pywws/services/openweathermap.ini
@@ -20,7 +20,6 @@
 url		= http://openweathermap.org/data/post
 catchup		= 7
 result		= ['{"message":"","cod":"200","id":"\d{5}"}$']
-rapidfire	= False
 use get		= False
 
 [fixed]

pywws/services/pwsweather.ini
@@ -20,7 +20,6 @@
 url = http://www.pwsweather.com/pwsupdate/pwsupdate.php
 catchup = 0
 use get = True
-rapidfire = False
 result = ['\n', '<html>\n', '<head>\n', '   <title>PWS Weather Station Update</title>\n', '</head>\n', '<body>\n', 'Data Logged and posted in METAR mirror.\n', '\n', '</body>\n', '</html>']
 
 [fixed]

pywws/services/stacjapogodywawpl.ini
@@ -20,7 +20,6 @@
 url		= http://stacjapogody.waw.pl/mapastacji/uploadweatherstationdata.php
 url-rf		= http://stacjapogody.waw.pl/mapastacji/uploadweatherstationdata.php
 catchup		= 7
-rapidfire	= True
 use get		= True
 result		= []
 

pywws/services/temperaturnu.ini
@@ -19,7 +19,6 @@
 [config]
 url		= http://www.temperatur.nu/rapportera.php
 catchup		= 0
-rapidfire	= False
 use get		= True
 result		= []
 

commit e242c240dd6203b1cb50e6c3323beca6b2fcc590

Extracted common parts of 'live' and other tasks.
Also found a bug introduced in commit ca0d137 that stopped 'live' data
being uploaded to services (they'd get the most recent logged data
instead).

pywws/Tasks.py
@@ -106,37 +106,45 @@ class RegularTasks(object):
             else:
                 yield template, ''
 
-    def do_live(self, data):
-        data = self.calibrator.calib(data)
+    def _do_common(self, sections, live_data=None):
         OK = True
-        yowindow_file = self.params.get('live', 'yowindow', '')
-        if yowindow_file:
-            self.yowindow.write_file(yowindow_file, data)
-        for template, flags in self._parse_templates('live', 'twitter'):
-            if not self.do_twitter(template, data):
-                OK = False
-        for service in eval(self.params.get('live', 'services', '[]')):
-            self.services[service].Upload(data)
+        for section in sections:
+            yowindow_file = self.params.get(section, 'yowindow', '')
+            if yowindow_file:
+                self.yowindow.write_file(yowindow_file)
+                break
+        for section in sections:
+            for template, flags in self._parse_templates(section, 'twitter'):
+                if not self.do_twitter(template):
+                    OK = False
+        all_services = []
+        for section in sections:
+            for service in eval(self.params.get(section, 'services', '[]')):
+                if service not in all_services:
+                    all_services.append(service)
+        for service in all_services:
+            self.services[service].Upload(live_data is None, live_data)
         uploads = []
         local_files = []
-        for template, flags in self._parse_templates('live', 'plot'):
-            upload = self.do_plot(template)
-            if not upload:
-                continue
-            if 'L' in flags:
-                local_files.append(upload)
-            else:
-                uploads.append(upload)
-        for template, flags in self._parse_templates('live', 'text'):
-            if 'T' in flags:
-                if not self.do_twitter(template, data):
-                    OK = False
-                continue
-            upload = self.do_template(template, data)
-            if 'L' in flags:
-                local_files.append(upload)
-            else:
-                uploads.append(upload)
+        for section in sections:
+            for template, flags in self._parse_templates(section, 'text'):
+                if 'T' in flags:
+                    if not self.do_twitter(template, live_data):
+                        OK = False
+                    continue
+                upload = self.do_template(template, live_data)
+                if 'L' in flags:
+                    local_files.append(upload)
+                else:
+                    uploads.append(upload)
+            for template, flags in self._parse_templates(section, 'plot'):
+                upload = self.do_plot(template)
+                if not upload:
+                    continue
+                if 'L' in flags:
+                    local_files.append(upload)
+                else:
+                    uploads.append(upload)
         if local_files:
             if not os.path.isdir(self.local_dir):
                 os.makedirs(self.local_dir)
@@ -148,6 +156,9 @@ class RegularTasks(object):
             for file in uploads:
                 os.unlink(file)
         return OK
+
+    def do_live(self, data):
+        return self._do_common(['live'], self.calibrator.calib(data))
 
     def do_tasks(self):
         sections = ['logged']
@@ -186,54 +197,7 @@ class RegularTasks(object):
             if (not last_update) or (last_update < threshold):
                 # time to do daily tasks
                 sections.append('daily')
-        OK = True
-        for section in sections:
-            for template, flags in self._parse_templates(section, 'twitter'):
-                if not self.do_twitter(template):
-                    OK = False
-        for section in sections:
-            yowindow_file = self.params.get(section, 'yowindow', '')
-            if yowindow_file:
-                self.yowindow.write_file(yowindow_file)
-                break
-        all_services = list()
-        for section in sections:
-            for service in eval(self.params.get(section, 'services', '[]')):
-                if service not in all_services:
-                    all_services.append(service)
-        for service in all_services:
-            self.services[service].Upload(True)
-        uploads = []
-        local_files = []
-        for section in sections:
-            for template, flags in self._parse_templates(section, 'plot'):
-                upload = self.do_plot(template)
-                if not upload:
-                    continue
-                if 'L' in flags:
-                    local_files.append(upload)
-                else:
-                    uploads.append(upload)
-            for template, flags in self._parse_templates(section, 'text'):
-                if 'T' in flags:
-                    if not self.do_twitter(template):
-                        OK = False
-                    continue
-                upload = self.do_template(template)
-                if 'L' in flags:
-                    local_files.append(upload)
-                else:
-                    uploads.append(upload)
-        if local_files:
-            if not os.path.isdir(self.local_dir):
-                os.makedirs(self.local_dir)
-            for file in local_files:
-                shutil.move(file, self.local_dir)
-        if uploads:
-            if not Upload.Upload(self.params, uploads):
-                OK = False
-            for file in uploads:
-                os.unlink(file)
+        OK = self._do_common(sections)
         if OK:
             for section in sections:
                 self.status.set('last update', section, now.isoformat(' '))

pywws/toservice.py
@@ -289,88 +289,43 @@ class ToService(object):
         self.use_get = eval(service_params.get('config', 'use get'))
         self.expected_result = eval(service_params.get('config', 'result'))
 
-    def translate_data(self, current, fixed_data):
-        """Convert a weather data record to upload format.
-
-        The :obj:`current` parameter contains the data to be uploaded.
-        It should be a 'calibrated' data record, as stored in
-        :class:`pywws.DataStore.calib_store`.
-
-        The :obj:`fixed_data` parameter contains unvarying data that
-        is site dependent, for example an ID code and authentication
-        data.
-
-        :param current: the weather data record.
-
-        :type current: dict
-
-        :param fixed_data: unvarying upload data.
-
-        :type fixed_data: dict
-
-        :return: converted data, or :obj:`None` if invalid data.
-
-        :rtype: dict(string)
-        
-        """
-        # check we have enough data
-        if current['temp_out'] is None or current['hum_out'] is None:
-            return None
-        # convert data
-        result = dict(fixed_data)
-        template_data = self.templater.make_text(self.template_file, current)
-        result.update(eval(template_data))
-        return result
-
-    def send_data(self, data, server, fixed_data):
+    def send_data(self, data):
         """Upload a weather data record.
 
         The :obj:`data` parameter contains the data to be uploaded.
         It should be a 'calibrated' data record, as stored in
         :class:`pywws.DataStore.calib_store`.
 
-        The :obj:`fixed_data` parameter contains unvarying data that
-        is site dependent, for example an ID code and authentication
-        data.
-
         :param data: the weather data record.
 
         :type data: dict
-
-        :param server: web address to upload to.
-
-        :type server: string
-
-        :param fixed_data: unvarying upload data.
-
-        :type fixed_data: dict
 
         :return: success status
 
         :rtype: bool
         
         """
-        coded_data = self.translate_data(data, fixed_data)
-        if not coded_data:
+        # check we have enough data
+        if data['temp_out'] is None or data['hum_out'] is None:
             return True
+        # convert data
+        coded_data = eval(self.templater.make_text(self.template_file, data))
+        coded_data.update(self.fixed_data)
         self.logger.debug(coded_data)
         coded_data = urllib.urlencode(coded_data)
         # have three tries before giving up
         for n in range(3):
             try:
-                if sys.hexversion <= 0x020406ff:
-                    wudata = urllib.urlopen('%s?%s' % (server, coded_data))
-                else:
-                    try:
-                        if self.use_get:
-                            wudata = urllib2.urlopen(
-                                '%s?%s' % (server, coded_data))
-                        else:
-                            wudata = urllib2.urlopen(server, coded_data)
-                    except urllib2.HTTPError, ex:
-                        if ex.code != 400:
-                            raise
-                        wudata = ex
+                try:
+                    if self.use_get:
+                        wudata = urllib2.urlopen(
+                            '%s?%s' % (self.server, coded_data))
+                    else:
+                        wudata = urllib2.urlopen(self.server, coded_data)
+                except urllib2.HTTPError, ex:
+                    if ex.code != 400:
+                        raise
+                    wudata = ex
                 response = wudata.readlines()
                 wudata.close()
                 if len(response) == len(self.expected_result):
@@ -391,7 +346,7 @@ class ToService(object):
                     self.old_ex = e
         return False
 
-    def Upload(self, catchup):
+    def Upload(self, catchup, live_data=None):
         """Upload one or more weather data records.
 
         This method uploads either the most recent weather data
@@ -424,18 +379,23 @@ class ToService(object):
                 start = max(start, last_update + timedelta(minutes=1))
             count = 0
             for data in self.data[start:]:
-                if not self.send_data(data, self.server, self.fixed_data):
+                if not self.send_data(data):
                     return False
                 self.status.set('last update', self.service_name,
                                 data['idx'].isoformat(' '))
                 count += 1
             if count:
                 self.logger.info('%d records sent', count)
+        elif live_data:
+            last_update = live_data['idx']
+            if not self.send_data(live_data):
+                return False
+            self.status.set(
+                'last update', self.service_name, last_update.isoformat(' '))
         else:
             # upload most recent data
             last_update = self.data.before(datetime.max)
-            if not self.send_data(
-                    self.data[last_update], self.server, self.fixed_data):
+            if not self.send_data(self.data[last_update]):
                 return False
             self.status.set(
                 'last update', self.service_name, last_update.isoformat(' '))

pywws/version.py
@@ -1,3 +1,3 @@
 version = '13.06'
-release = '1021'
-commit = 'f1dc886'
+release = '1022'
+commit = 'e785f4e'