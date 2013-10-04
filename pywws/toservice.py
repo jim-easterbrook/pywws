jim-easterbrook/pywws

commit fdb985bd9d33298b8b64257cb5b311258bfe1cac

Suppress 'underground' if 'underground_rf' in use.
Previously if you have 'underground' in your [logged] services and
'underground_rf' (RapidFire) in your [live] services then both types of
upload would be used. Weather Underground don't say not to do this, but
it is an overhead sending near-duplicate data.

Now, by careful fiddling with 'last update' timestamps, 'underground' is
only used for catchup if your station goes off line for any reason. Once
the catchup data has been sent only 'underground_rf' is used.

pywws/toservice.py
@@ -226,6 +226,7 @@ from pywws.Logger import ApplicationLogger
 from pywws import Template
 
 FIVE_MINS = timedelta(minutes=5)
+FIFTY_SECS = timedelta(seconds=50)
 
 class ToService(object):
     """Upload weather data to weather services such as Weather
@@ -260,6 +261,10 @@ class ToService(object):
         # 'derived' services such as 'underground_rf' share their
         # parent's config and templates
         config_section = self.service_name.split('_')[0]
+        if config_section == self.service_name:
+            self.parent = None
+        else:
+            self.parent = config_section
         self.old_response = None
         self.old_ex = None
         # set default socket timeout, so urlopen calls don't hang forever
@@ -389,13 +394,22 @@ class ToService(object):
             else:
                 start = self.data.before(datetime.max)
         if live_data:
-            stop = live_data['idx'] - timedelta(seconds=48)
+            stop = live_data['idx'] - FIFTY_SECS
         else:
             stop = None
         for data in self.data[start:stop]:
             yield data
         if live_data:
             yield live_data
+
+    def set_status(self, timestamp):
+        self.status.set(
+            'last update', self.service_name, timestamp.isoformat(' '))
+        if self.parent:
+            last_update = self.status.get_datetime('last update', self.parent)
+            if last_update and last_update >= timestamp - FIVE_MINS:
+                self.status.set(
+                    'last update', self.parent, timestamp.isoformat(' '))
 
     def catchup_start(self):
         """Get datetime of first 'catchup' record to send.
@@ -405,7 +419,7 @@ class ToService(object):
         """
         if self.catchup <= 0:
             return None
-        start = datetime.utcnow() - timedelta(days=self.catchup)
+        start = datetime.utcnow() - FIFTY_SECS
         last_update = self.params.get_datetime(self.service_name, 'last update')
         if last_update:
             self.params.unset(self.service_name, 'last update')
@@ -413,7 +427,11 @@ class ToService(object):
                             last_update.isoformat(' '))
         last_update = self.status.get_datetime('last update', self.service_name)
         if last_update:
-            start = max(start, last_update + timedelta(seconds=30))
+            start = max(start, last_update + FIFTY_SECS)
+        if self.parent:
+            last_update = self.status.get_datetime('last update', self.parent)
+            if last_update:
+                start = max(start, last_update + FIFTY_SECS)
         return start
 
     def Upload(self, catchup=True, live_data=None):
@@ -446,8 +464,7 @@ class ToService(object):
                 continue
             if not self.send_data(coded_data):
                 return False
-            self.status.set('last update', self.service_name,
-                            data['idx'].isoformat(' '))
+            self.set_status(data['idx'])
             count += 1
         if count > 1:
             self.logger.info('%d records sent', count)

pywws/version.py
@@ -1,3 +1,3 @@
 version = '13.09'
-release = '1077'
-commit = '59563d0'
+release = '1078'
+commit = '8ecd4fa'

pywws/Tasks.py
@@ -32,7 +32,7 @@ from pywws.calib import Calib
 from pywws import Plot
 from pywws import Template
 from pywws.TimeZone import Local
-from pywws.toservice import ToService
+from pywws.toservice import ToService, FIVE_MINS, FIFTY_SECS
 from pywws import Upload
 from pywws import WindRose
 from pywws import YoWindow
@@ -266,7 +266,7 @@ class RegularTasks(object):
                     break
                 if message[0] == 'service':
                     name, timestamp = message[1:]
-                    self.status.set('last update', name, timestamp)
+                    self.services[name].set_status(timestamp)
                     self.service_queued[name] -= 1
         return OK
 
@@ -351,9 +351,14 @@ class RegularTasks(object):
                 coded_data = service.encode_data(data)
                 if not coded_data:
                     continue
-                self.to_thread.put(
-                    ('service', name, data['idx'].isoformat(' '), coded_data))
-                self.service_start[name] = data['idx'] + timedelta(seconds=30)
+                self.to_thread.put(('service', name, data['idx'], coded_data))
+                self.service_start[name] = data['idx'] + FIFTY_SECS
+                parent = service.parent
+                if parent:
+                    last_update = self.status.get_datetime(
+                        'last update', parent)
+                    if last_update and last_update >= data['idx'] - FIVE_MINS:
+                        self.service_start[parent] = data['idx'] + FIFTY_SECS
                 self.service_queued[name] += 1
         else:
             service.Upload(live_data=live_data)

commit 94bbbfbcf4b80a3c0103fcf2ef117b4cfc370986

Fix bug introduced in commit fdb985b.
'Catchup' wasn't happening as I'd messed up the computation of first
catchup record to send.

pywws/toservice.py
@@ -419,7 +419,7 @@ class ToService(object):
         """
         if self.catchup <= 0:
             return None
-        start = datetime.utcnow() - FIFTY_SECS
+        start = datetime.utcnow() - timedelta(days=self.catchup)
         last_update = self.params.get_datetime(self.service_name, 'last update')
         if last_update:
             self.params.unset(self.service_name, 'last update')

pywws/version.py
@@ -1,3 +1,3 @@
 version = '13.10'
-release = '1079'
-commit = 'fdb985b'
+release = '1080'
+commit = '35e47d3'