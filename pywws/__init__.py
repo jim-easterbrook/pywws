jim-easterbrook/pywws

commit fd80aa36863ec526c5e869d8afdd38eaad3cca27

Bring version numbering in line with PEP 440.
PEP 440 (http://www.python.org/dev/peps/pep-0440/) proposes a standard
for Python package version numbering. Its idea of developmental releases
is close to how pywws evolves. It doesn't approve of date based version
numbers though.

pywws/__init__.py
@@ -16,5 +16,5 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from pywws import version
-__version__ = '%s_r%s' % (version.version, version.release)
+from pywws.version import version
+__version__ = version

pywws/version.py
@@ -1,5 +1,5 @@
-version = '13.11'
-release = '1109'
-commit = '3880d37'
+version = '13.11.dev1110'
+release = '1110'
+commit = '660cf6d'
 if __name__ == '__main__':
-    print '%s_r%s' % (version, release)
+    print(version)

setup.py
@@ -33,26 +33,27 @@ import subprocess
 import pywws.version
 
 # regenerate version file, if required
+regenerate = False
 try:
-    p = subprocess.Popen(
-        ['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     commit = p.communicate()[0].strip().decode('ASCII')
-    if p.returncode:
-        commit = pywws.version.commit
+    regenerate = (not p.returncode) and commit != pywws.version.commit
 except OSError:
-    commit = pywws.version.commit
-if commit != pywws.version.commit:
-    pywws.version.version = date.today().strftime('%y.%m')
-    pywws.version.release = str(int(pywws.version.release) + 1)
-    pywws.version.commit = commit
+    pass
+if regenerate:
+    release = str(int(pywws.version.release) + 1)
+    version = date.today().strftime('%y.%m') + '.dev%s' % release
     vf = open('pywws/version.py', 'w')
     vf.write("""version = '%s'
 release = '%s'
 commit = '%s'
 if __name__ == '__main__':
-    print '%%s_r%%s' %% (version, release)
-""" % (pywws.version.version, pywws.version.release, pywws.version.commit))
+    print(version)
+""" % (version, release, commit))
     vf.close()
+else:
+    version = pywws.version.version
 
 # Custom distutils classes
 class xgettext(Command):
@@ -90,7 +91,7 @@ class xgettext(Command):
         options = [
             '--language=Python', '--no-wrap',
             '--copyright-holder="Jim Easterbrook"', '--package-name=pywws',
-            '--package-version=%s' % pywws.version.version,
+            '--package-version=%s' % version,
             '--msgid-bugs-address="jim@jim-easterbrook.me.uk"',
             '--output=%s' % self.output_file,
             ]
@@ -264,8 +265,6 @@ for root, dirs, files in os.walk('examples'):
         paths.append(os.path.join(root, name))
     if paths:
         data_files.append(('share/pywws/%s' % root, paths))
-
-version = '%s_r%s' % (pywws.version.version, pywws.version.release)
 
 if using_setuptools:
     setup_kw['include_package_data'] = True

commit 4ce6ac82ec1ce39cc41c9ba8891c1c35aa1c9d37

Fix bug introduced in commit fd80aa3.
As far as I know the bug only affects use of setup.py, so shouldn't be a
problem to most users.

pywws/__init__.py
@@ -16,5 +16,5 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from pywws.version import version
-__version__ = version
+import pywws.version
+__version__ = pywws.version.version

pywws/version.py
@@ -1,5 +1,5 @@
-version = '13.11.dev1110'
-release = '1110'
-commit = '660cf6d'
+version = '13.11.dev1111'
+release = '1111'
+commit = 'fd80aa3'
 if __name__ == '__main__':
     print(version)
4ce6ac82ec1ce39cc41c9ba8891c1c35aa1c9d37
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2013-11-20 17:54:55 +0000

    Fix bug introduced in commit fd80aa3.
    As far as I know the bug only affects use of setup.py, so shouldn't be a
    problem to most users.

--- pywws/__init__.py
+++ pywws/__init__.py
@@ -16,5 +16,5 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from pywws.version import version
-__version__ = version
+import pywws.version
+__version__ = pywws.version.version
--- pywws/version.py
+++ pywws/version.py
@@ -1,5 +1,5 @@
-version = '13.11.dev1110'
-release = '1110'
-commit = '660cf6d'
+version = '13.11.dev1111'
+release = '1111'
+commit = 'fd80aa3'
 if __name__ == '__main__':
     print(version)
