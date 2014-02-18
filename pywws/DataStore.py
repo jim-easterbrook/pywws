jim-easterbrook/pywws

commit 0fec37d1774c82b72a388c0b4a5bafba4c663e02

Made params and status storage threadsafe.

pywws/DataStore.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2008-13  Jim Easterbrook  jim@jim-easterbrook.me.uk
+# Copyright (C) 2008-14  Jim Easterbrook  jim@jim-easterbrook.me.uk
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -68,11 +68,14 @@ Detailed API
 
 """
 
+from __future__ import with_statement
+
 from ConfigParser import RawConfigParser
 import csv
 from datetime import date, datetime, timedelta, MAXYEAR
 import os
 import sys
+from threading import Lock
 import time
 
 DAY = timedelta(days=1)
@@ -91,13 +94,15 @@ def safestrptime(date_string, format=None):
 
 class ParamStore(object):
     def __init__(self, root_dir, file_name):
-        if not os.path.isdir(root_dir):
-            os.makedirs(root_dir)
-        self._path = os.path.join(root_dir, file_name)
-        self._dirty = False
-        # open config file
-        self._config = RawConfigParser()
-        self._config.read(self._path)
+        self._lock = Lock()
+        with self._lock:
+            if not os.path.isdir(root_dir):
+                os.makedirs(root_dir)
+            self._path = os.path.join(root_dir, file_name)
+            self._dirty = False
+            # open config file
+            self._config = RawConfigParser()
+            self._config.read(self._path)
 
     def __del__(self):
         self.flush()
@@ -105,10 +110,11 @@ class ParamStore(object):
     def flush(self):
         if not self._dirty:
             return
-        self._dirty = False
-        of = open(self._path, 'w')
-        self._config.write(of)
-        of.close()
+        with self._lock:
+            self._dirty = False
+            of = open(self._path, 'w')
+            self._config.write(of)
+            of.close()
 
     def get(self, section, option, default=None):
         """Get a parameter value and return a string.
@@ -118,11 +124,12 @@ class ParamStore(object):
         then the return value.
 
         """
-        if not self._config.has_option(section, option):
-            if default is not None:
-                self.set(section, option, default)
-            return default
-        return self._config.get(section, option)
+        with self._lock:
+            if not self._config.has_option(section, option):
+                if default is not None:
+                    self.set(section, option, default)
+                return default
+            return self._config.get(section, option)
 
     def get_datetime(self, section, option, default=None):
         result = self.get(section, option, default)
@@ -132,24 +139,26 @@ class ParamStore(object):
 
     def set(self, section, option, value):
         """Set option in section to string value."""
-        if not self._config.has_section(section):
-            self._config.add_section(section)
-        elif (self._config.has_option(section, option) and
-              self._config.get(section, option) == value):
-            return
-        self._config.set(section, option, value)
-        self._dirty = True
+        with self._lock:
+            if not self._config.has_section(section):
+                self._config.add_section(section)
+            elif (self._config.has_option(section, option) and
+                  self._config.get(section, option) == value):
+                return
+            self._config.set(section, option, value)
+            self._dirty = True
 
     def unset(self, section, option):
         """Remove option from section."""
-        if not self._config.has_section(section):
-            return
-        if self._config.has_option(section, option):
-            self._config.remove_option(section, option)
-            self._dirty = True
-        if not self._config.options(section):
-            self._config.remove_section(section)
-            self._dirty = True
+        with self._lock:
+            if not self._config.has_section(section):
+                return
+            if self._config.has_option(section, option):
+                self._config.remove_option(section, option)
+                self._dirty = True
+            if not self._config.options(section):
+                self._config.remove_section(section)
+                self._dirty = True
 
 class params(ParamStore):
     def __init__(self, root_dir):

pywws/version.py
@@ -1,5 +1,5 @@
-version = '14.02.dev1149'
-release = '1149'
-commit = 'a4e0ddf'
+version = '14.02.dev1150'
+release = '1150'
+commit = '81ee0aa'
 if __name__ == '__main__':
     print(version)

commit bf8c27c8eaa1be05b824f319598e79055887e9fc

Fix bug introduced in commit 0fec37d177.
The params/status stores were locking up when attempting to create a new
config item. "get" set the lock which "set" then waited for.

pywws/DataStore.py
@@ -127,7 +127,7 @@ class ParamStore(object):
         with self._lock:
             if not self._config.has_option(section, option):
                 if default is not None:
-                    self.set(section, option, default)
+                    self._set(section, option, default)
                 return default
             return self._config.get(section, option)
 
@@ -140,13 +140,16 @@ class ParamStore(object):
     def set(self, section, option, value):
         """Set option in section to string value."""
         with self._lock:
-            if not self._config.has_section(section):
-                self._config.add_section(section)
-            elif (self._config.has_option(section, option) and
-                  self._config.get(section, option) == value):
-                return
-            self._config.set(section, option, value)
-            self._dirty = True
+            self._set(section, option, value)
+
+    def _set(self, section, option, value):
+        if not self._config.has_section(section):
+            self._config.add_section(section)
+        elif (self._config.has_option(section, option) and
+              self._config.get(section, option) == value):
+            return
+        self._config.set(section, option, value)
+        self._dirty = True
 
     def unset(self, section, option):
         """Remove option from section."""

pywws/version.py
@@ -1,5 +1,5 @@
-version = '14.02.dev1153'
-release = '1153'
-commit = '5d592c3'
+version = '14.02.dev1154'
+release = '1154'
+commit = '650e450'
 if __name__ == '__main__':
     print(version)