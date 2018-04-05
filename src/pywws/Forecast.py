9a825ba9c4b22a52d6f6e694419afef1e2043418
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2018-04-05 15:46:21 +0100

    Merge ZambrettiCore into Forecast
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/pywws/Forecast.py
+++ src/pywws/Forecast.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
 # Copyright (C) 2008-18  pywws contributors
@@ -44,9 +42,47 @@ import sys
 from pywws import DataStore
 from pywws import Localisation
 from pywws.TimeZone import Local, utc
-from pywws import ZambrettiCore
+
+def _(msg):
+    return msg
+
+_forecast_text = {
+    'A' : _("Settled fine"),
+    'B' : _("Fine weather"),
+    'C' : _("Becoming fine"),
+    'D' : _("Fine, becoming less settled"),
+    'E' : _("Fine, possible showers"),
+    'F' : _("Fairly fine, improving"),
+    'G' : _("Fairly fine, possible showers early"),
+    'H' : _("Fairly fine, showery later"),
+    'I' : _("Showery early, improving"),
+    'J' : _("Changeable, mending"),
+    'K' : _("Fairly fine, showers likely"),
+    'L' : _("Rather unsettled clearing later"),
+    'M' : _("Unsettled, probably improving"),
+    'N' : _("Showery, bright intervals"),
+    'O' : _("Showery, becoming less settled"),
+    'P' : _("Changeable, some rain"),
+    'Q' : _("Unsettled, short fine intervals"),
+    'R' : _("Unsettled, rain later"),
+    'S' : _("Unsettled, some rain"),
+    'T' : _("Mostly very unsettled"),
+    'U' : _("Occasional rain, worsening"),
+    'V' : _("Rain at times, very unsettled"),
+    'W' : _("Rain at frequent intervals"),
+    'X' : _("Rain, very unsettled"),
+    'Y' : _("Stormy, may improve"),
+    'Z' : _("Stormy, much rain")
+    }
+
+del _
+
 
 def ZambrettiCode(params, hourly_data):
+    """Simple implementation of Zambretti forecaster algorithm.
+    Inspired by beteljuice.com Java algorithm, as converted to Python by
+    honeysucklecottage.me.uk, and further information
+    from http://www.meteormetrics.com/zambretti.htm"""
     north = eval(params.get('Zambretti', 'north', 'True'))
     baro_upper = eval(params.get('Zambretti', 'baro upper', '1050.0'))
     baro_lower = eval(params.get('Zambretti', 'baro lower', '950.0'))
@@ -60,13 +96,50 @@ def ZambrettiCode(params, hourly_data):
         trend = 0.0
     else:
         trend = hourly_data['pressure_trend'] / 3.0
-    return ZambrettiCore.ZambrettiCode(
-        hourly_data['rel_pressure'], hourly_data['idx'].month, wind, trend,
-        north=north, baro_top=baro_upper, baro_bottom=baro_lower)
+    # normalise pressure
+    pressure = 950.0 + (
+        (1050.0 - 950.0) * (hourly_data['rel_pressure'] - baro_lower) /
+        (baro_upper - baro_lower))
+    # adjust pressure for wind direction
+    if wind is not None:
+        if not isinstance(wind, int):
+            wind = int(wind + 0.5) % 16
+        if not north:
+            # southern hemisphere, so add 180 degrees
+            wind = (wind + 8) % 16
+        pressure += (  5.2,  4.2,  3.2,  1.05, -1.1, -3.15, -5.2, -8.35,
+                     -11.5, -9.4, -7.3, -5.25, -3.2, -1.15,  0.9,  3.05)[wind]
+    # compute base forecast from pressure and trend (hPa / hour)
+    summer = north == (hourly_data['idx'].month >= 4 and
+                       hourly_data['idx'].month <= 9)
+    if trend >= 0.1:
+        # rising pressure
+        if summer:
+            pressure += 3.2
+        F = 0.1740 * (1031.40 - pressure)
+        LUT = ('A', 'B', 'B', 'C', 'F', 'G', 'I', 'J', 'L', 'M', 'M', 'Q', 'T',
+               'Y')
+    elif trend <= -0.1:
+        # falling pressure
+        if summer:
+            pressure -= 3.2
+        F = 0.1553 * (1029.95 - pressure)
+        LUT = ('B', 'D', 'H', 'O', 'R', 'U', 'V', 'X', 'X', 'Z')
+    else:
+        # steady
+        F = 0.2314 * (1030.81 - pressure)
+        LUT = ('A', 'B', 'B', 'B', 'E', 'K', 'N', 'N', 'P', 'P', 'S', 'W', 'W',
+               'X', 'X', 'X', 'Z')
+    # clip to range of lookup table
+    F = min(max(int(F + 0.5), 0), len(LUT) - 1)
+    # convert to letter code
+    return LUT[F]
+
 
 def Zambretti(params, hourly_data):
     code = ZambrettiCode(params, hourly_data)
-    return Localisation.translation.ugettext(ZambrettiCore.ZambrettiText(code))
+    return Localisation.translation.ugettext(_forecast_text[code])
+
 
 def main(argv=None):
     if argv is None:
--- src/pywws/__init__.py
+++ src/pywws/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '18.04.1'
-_release = '1409'
-_commit = 'a575953'
+_release = '1410'
+_commit = 'd8352d9'
