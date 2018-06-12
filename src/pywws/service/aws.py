3d0cbee515defed75c70ed68ff424a77e5b9d14a
Chris Ramsay <chris@ramsay-family.net>
2018-06-12 16:41:37 +0000

    First stab at a service

---
+++ src/pywws/service/aws.py
@@ -0,0 +1,153 @@
+# pywws - Python software for USB Wireless Weather Stations
+# http://github.com/jim-easterbrook/pywws
+# Copyright (C) 2018  pywws contributors
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""Upload weather data to UK Met Office "WOW".
+
+The UK Met Office runs a `Weather Observations Website`_ (WOW) that
+displays readings from amateur and official weather stations. This
+module uploads data to it from pywws. You can upload "logged" or "live"
+data (or both). The module ensures there is at least 5 minutes between
+each reading as required by the API.
+
+* Create account: https://register.metoffice.gov.uk/WaveRegistrationClient/public/newaccount.do?service=weatherobservations
+* API: http://wow.metoffice.gov.uk/support/dataformats#automatic
+* Example ``weather.ini`` configuration::
+
+    [metoffice]
+    site id = 12345678
+    aws pin = 987654
+
+    [logged]
+    services = ['metoffice', 'underground']
+
+    [live]
+    services = ['metoffice', 'underground']
+
+Note that a ``site id`` allocated since June 2016 will probably look
+like ``6a571450-df53-e611-9401-0003ff5987fd``.
+
+.. _Weather Observations Website: http://wow.metoffice.gov.uk/home
+
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from contextlib import contextmanager
+from datetime import timedelta
+import logging
+import os
+import sys
+
+import requests
+
+import pywws
+from pywws.conversions import rain_inch
+import pywws.service
+
+__docformat__ = "restructuredtext en"
+service_name = os.path.splitext(os.path.basename(__file__))[0]
+logger = logging.getLogger(__name__)
+
+
+class ToService(pywws.service.BaseToService):
+    catchup = 100
+    fixed_data = {}
+    interval = timedelta(seconds=300)
+    logger = logger
+    service_name = service_name
+    template = """
+#live#
+#hum_in       "'hum_in'     : '%.d',"#
+#temp_in      "'temp_in'    : '%.1f',"#
+#hum_out      "'hum_out'    : '%.d',"#
+#temp_out     "'temp_out'   : '%.1f',"#
+#calc "dew_point(data['temp_out'], data['hum_out'])" "'temp_dewpt'  : '%.1f',"#
+#rel_pressure "'abs_pressure'   : '%.4f',"#
+#wind_ave     "'wind_ave'   : '%.2f'," "" "wind_mph(x)"#
+#wind_gust    "'wind_gust'  : '%.2f'," "" "wind_mph(x)"#
+#wind_dir     "'wind_dir'   : '%.0f'," "" "winddir_degrees(x)"#
+#calc "rain_hour(data)" "'rain'     : '%g',"#
+#calc "rain_day(data)" "'rain_day'     : '%g',"#
+#calc "wind_chill(data['temp_out'], data['wind_ave'])" "'wind_chill'	: '%.1f',"#
+#calc "apparent_temp(data['temp_out'], data['hum_out'], data['wind_ave'])" "'temp_apprt'	:  '%.1f',"#
+#calc "cloud_base(data['temp_out'], data['hum_out'])" "'cloud_base' : '%.1f',"#
+#idx          "'tdate'      : '%Y-%m-%d',"#
+#idx          "'ttime'      : '%H:%M:%S',"#
+
+"""
+
+    def __init__(self, context):
+        # initialise rain history
+        last_update = context.status.get_datetime('last update', service_name)
+        if last_update:
+            last_update = context.calib_data.nearest(last_update)
+            self.last_rain = context.calib_data[last_update]['rain']
+        else:
+            self.last_rain = None
+        # base class init
+        super(ToService, self).__init__(context)
+
+    @contextmanager
+    def session(self):
+        with requests.Session() as session:
+            yield session
+
+    def prepare_data(self, data):
+        prepared_data = super(ToService, self).prepare_data(data)
+        # compute rain since last upload
+        if self.last_rain is not None:
+            rain = data['rain'] - self.last_rain
+            if rain >= -0.001:
+                prepared_data['rain_day'] = '{:.1f}'.format(rain)
+        self.last_rain = data['rain']
+        # compute rain since day start
+        day_start = self.context.daily_data.nearest(data['idx'])
+        day_start = self.context.daily_data[day_start]['start']
+        day_start = self.context.calib_data.after(day_start)
+        rain = data['rain'] - self.context.calib_data[day_start]['rain']
+        if rain >= -0.001:
+            prepared_data['rain_day'] = '{:.1f}'.format(rain)
+        return prepared_data
+
+    def valid_data(self, data):
+        return any([data[x] is not None for x in (
+            'wind_dir', 'wind_ave', 'wind_gust', 'hum_out', 'temp_out',
+            'rel_pressure')])
+
+    def upload_data(self, session, prepared_data={}, live=False):
+        try:
+            sec = {'x-api-key': 'KEY'}
+	    session.headers.update(sec)
+            rsp = session.get('URL',
+                              params=prepared_data, timeout=60)
+        except Exception as ex:
+            return False, str(ex)
+        if rsp.status_code == 429:
+            # UK Met Office server uses 429 to signal duplicate data
+            return True, 'repeated data {:s}'.format(prepared_data['dateutc'])
+        if rsp.status_code != 200:
+            return False, 'http status: {:d}'.format(rsp.status_code)
+        rsp = rsp.json()
+        if rsp:
+            return True, 'server response "{!r}"'.format(rsp)
+        return True, 'OK'
+
+
+if __name__ == "__main__":
+    sys.exit(pywws.service.main(ToService))
+
