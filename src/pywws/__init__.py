jim-easterbrook/pywws

commit 706e59fb9674d133c885c1ec937862b12cf4d7f8

Allow service.session factory to return an error
Most services create sessions without any error-prone process but some,
such as CWOP, can fail. This allows cleaner error reporting when that
happens.

Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

src/pywws/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '22.9.0'
-_release = '1696'
-_commit = 'e9ef20a'
+__version__ = '22.10.0'
+_release = '1697'
+_commit = '34d3ef1'

src/pywws/service/__init__.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018-20  pywws contributors
+# Copyright (C) 2018-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -345,8 +345,10 @@ class CatchupDataService(DataServiceBase):
     def upload_batch(self):
         OK = True
         count = 0
-        with self.session() as session:
-            while self.queue and not self.context.shutdown.is_set():
+        with self.session() as (session, message):
+            if not session:
+                self.log(message)
+            while session and self.queue and not self.context.shutdown.is_set():
                 # send upload without taking it off queue
                 upload = self.queue[0]
                 if upload is None:
@@ -403,9 +405,12 @@ class LiveDataService(DataServiceBase):
         # check time since last upload
         if timestamp and timestamp < self.last_update + self.interval:
             return True
-        with self.session() as session:
-            OK, message = self.upload_data(session, prepared_data=prepared_data)
-        self.log(message)
+        OK = False
+        with self.session() as (session, message):
+            if session:
+                OK, message = self.upload_data(
+                    session, prepared_data=prepared_data)
+            self.log(message)
         if OK:
             self.logger.info('1 record sent')
             if timestamp:
@@ -435,8 +440,10 @@ class FileService(ServiceBase):
             self.context.status.get('pending', self.service_name, '[]'))
         OK = True
         count = 0
-        with self.session() as session:
-            while self.queue and not self.context.shutdown.is_set():
+        with self.session() as (session, message):
+            if not session:
+                self.log(message)
+            while session and self.queue and not self.context.shutdown.is_set():
                 upload = self.queue[0]
                 if upload is None:
                     OK = False

src/pywws/service/copy.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2008-18  pywws contributors
+# Copyright (C) 2008-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -74,7 +74,7 @@ class ToService(pywws.service.FileService):
 
     @contextmanager
     def session(self):
-        yield None
+        yield None, 'OK'
 
     def upload_file(self, session, path):
         try:

src/pywws/service/cwop.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018  pywws contributors
+# Copyright (C) 2018-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -105,7 +105,7 @@ class ToService(pywws.service.LiveDataService):
             session.connect((server, 14580))
             response = session.recv(4096).decode('ASCII')
             logger.debug('server software: %s', response.strip())
-            yield session
+            yield session, 'OK'
 
     def upload_data(self, session, prepared_data={}):
         login = ('user {designator:s} pass {passcode:s} ' +

src/pywws/service/ftp.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2008-18  pywws contributors
+# Copyright (C) 2008-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -91,7 +91,7 @@ class ToService(pywws.service.FileService):
             logger.debug('welcome message\n' + session.getwelcome())
             session.login(self.params['user'], self.params['password'])
             session.cwd(self.params['directory'])
-            yield session
+            yield session, 'OK'
 
     def upload_file(self, session, path):
         target = os.path.basename(path)

src/pywws/service/mastodon.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018  pywws contributors
+# Copyright (C) 2018-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -184,7 +184,7 @@ class ToService(pywws.service.FileService):
     @contextmanager
     def session(self):
         yield Mastodon(access_token=self.params['access_token'],
-                       api_base_url=self.params['handle'].split('@')[-1])
+                       api_base_url=self.params['handle'].split('@')[-1]), 'OK'
 
     def upload_file(self, session, filename):
         with codecs.open(filename, 'r', encoding=self.encoding) as toot_file:

src/pywws/service/metoffice.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018-21  pywws contributors
+# Copyright (C) 2018-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -105,7 +105,7 @@ class ToService(pywws.service.CatchupDataService):
     @contextmanager
     def session(self):
         with requests.Session() as session:
-            yield session
+            yield session, 'OK'
 
     def rain_rate(self, data):
         # compute rain since last upload

src/pywws/service/mqtt.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018-20  pywws contributors
+# Copyright (C) 2018-22  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -253,7 +253,7 @@ class ToService(pywws.service.LiveDataService):
             session.tls_set(self.params['tls_cert'], tls_version=self.params['tls_ver'])
         session.connect(self.params['hostname'], self.params['port'])
         try:
-            yield session
+            yield session, 'OK'
         finally:
             session.disconnect()
 

commit b31dfc812901a49d106a6d66d1aa10cdf0da0770

Fix bug in service.copy
This was introduced in commit 706e59f which tests the yielded session
object to see if a service was successfully created. The copy service
doesn't need a session, but yielding 'None' fails the subsequent test.

Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

src/pywws/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '23.2.0'
-_release = '1702'
-_commit = 'bdc9d88'
+_release = '1703'
+_commit = 'e8dfaba'

src/pywws/service/__init__.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2018-22  pywws contributors
+# Copyright (C) 2018-23  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -159,7 +159,7 @@ class ServiceBase(threading.Thread):
 
         For a typical example, see the source code of the
         :py:mod:`pywws.service.openweathermap` module. If your upload
-        can't benefit from a session object yield :py:obj:`None`, as in
+        can't benefit from a session object yield :py:obj:`True`, as in
         :py:mod:`pywws.service.copy`.
         """
         raise NotImplementedError()

src/pywws/service/copy.py
@@ -1,6 +1,6 @@
 # pywws - Python software for USB Wireless Weather Stations
 # http://github.com/jim-easterbrook/pywws
-# Copyright (C) 2008-22  pywws contributors
+# Copyright (C) 2008-23  pywws contributors
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -74,7 +74,7 @@ class ToService(pywws.service.FileService):
 
     @contextmanager
     def session(self):
-        yield None, 'OK'
+        yield True, 'OK'
 
     def upload_file(self, session, path):
         try: